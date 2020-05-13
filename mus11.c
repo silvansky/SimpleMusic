921febd5f04aa8483ecc48b22316afa1830271db
Valentine Silvansky <v.silvansky@gmail.com>
2020-05-13 11:29:04 +0300

    Add 10 and 11

---
+++ mus11.c
@@ -0,0 +1,40 @@
+#include <math.h>
+
+// stereo, set channels to 2
+
+double baseFreq = 0.5;
+
+double wave1(t)
+{
+	return sin(t*baseFreq);
+}
+
+double wave2(t)
+{
+	return sin(t*baseFreq*2);
+}
+
+double wave3(t)
+{
+	return sin(t*baseFreq*3);
+}
+
+double wave4(t)
+{
+	return sin(t*baseFreq*4);
+}
+
+main(t)
+{
+	for (t=0;;t++)
+	{
+		// left
+		putchar((int)(
+			21*(wave1(t) + 0.5*wave2(t) + 0.3*wave3(t) + 0.2*wave4(t)) + 9000/exp(t/20000.)*wave4(t) - 20*exp(t/50000.)
+		));
+		// right
+		putchar((int)(
+			20*(wave1(t)-wave3(t-1)/2) - 9000/exp(t/20000.)*wave3(t) + 20*exp(t/55000.)
+		));
+	}
+}
